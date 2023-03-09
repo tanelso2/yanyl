79cc55801afa72852100b186b3163aa067650a77
Thomas Nelson <tanelso2@gmail.com>
2023-03-09 00:09:30 -0600

    fixup! Handle accent-quoted keys in objects properly

---
+++ tests/tyaml_accentquotes.nim
@@ -0,0 +1,33 @@
+import
+  macros,
+  unittest,
+  yanyl,
+  test_utils/yaml_testing
+
+type
+  Owner = object of RootObj
+    name: string
+    `addr`: string
+  Pet = ref object of RootObj
+    name*: string
+    `type`*: string
+    owner: Owner
+
+deriveYamls:
+  Owner
+  Pet
+
+let sample = """
+name: Garfield
+type: cat
+owner:
+    name: J. Arbuckle
+    addr: 711 Maple Street
+"""
+
+let garf = ofYamlStr(sample, Pet)
+check garf.name == "Garfield"
+check garf.`type` == "cat"
+check garf.owner.name == "J. Arbuckle"
+
+checkRoundTrip garf
